d5ddda088e40709d026b799d26c8f9eaa28a9efd
Kapshai <leonid.kapshai@gmail.com>
2022-11-21 00:07:54 +0300

    Вторая задача

---
+++ Z-13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99) //number < -99)
+{
+    string stringnumber = number.ToString();
+    Console.WriteLine(stringnumber[2]);
+}
+
+if (number < -99) 
+{
+    string stringnumber = number.ToString();
+    Console.WriteLine(stringnumber[3]);
+}
+if (number < 99 && number > -99)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
