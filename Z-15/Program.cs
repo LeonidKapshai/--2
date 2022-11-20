2927573d252dd9802fbee63ce5e0c63d259028c2
Kapshai <leonid.kapshai@gmail.com>
2022-11-21 00:40:57 +0300

    Три задачи

---
+++ Z-15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0 || number > 7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+if (number == 6)
+{
+    Console.WriteLine("Выходной");
+}
+if (number == 7)
+{
+    Console.WriteLine("Выходной");
+}
+if (number > 0 && number < 6)
+{
+   Console.WriteLine("Будний");
+}
