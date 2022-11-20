38ef732273a06128f9f2d32ebcdb3dcdf3fed386
Kapshai <leonid.kapshai@gmail.com>
2022-11-20 23:23:08 +0300

    1Задача

--- Z-10/Program.cs
+++ Z-10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > -1000 && number < 1000)
+{
+    number = (number % 100) / 10;
+    Console.WriteLine (number);
+}
+else 
+{
+    Console.WriteLine ("Число не трехзначное");
+}
